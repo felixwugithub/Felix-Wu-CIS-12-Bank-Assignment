6fe3d407dd81f2d580d5e8966b0847c62d66d95a
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-01-24 14:05:43 -0800

    Added tests

--- Bank-System/src/Main.java
+++ Bank-System/src/Main.java
@@ -6,7 +6,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        //tests on overdraft checking
+        //tests on overdraft checking & withdrawals
+
         Date date = Calendar.getInstance().getTime();
 
         Customer c1 = new Customer("John Cena", 3219813, 0.00, 0.00);
@@ -14,19 +15,24 @@ public class Main {
         c1.withdraw(0.01, date, Customer.CHEQUING);
         c1.withdraw(100.0, date, Customer.SAVING);
         c1.withdraw(0.01, date, Customer.SAVING);
-        c1.displayWithdraws();
-        c1.displayBalance(Customer.CHEQUING);
-        c1.displayBalance(Customer.SAVING);
 
+        c1.displayBalance(Customer.CHEQUING);   //expected output: -100.00
+        c1.displayBalance(Customer.SAVING);     //expected output: -100.00    (negative balance after overdrafting)
+        c1.withdraw(0.01, date, Customer.CHEQUING); //expected output: none, since maximum overdraft has been used up
+
+        c1.displayWithdraws();
+
+
+        //tests on deposits and initializing a customer object with initial deposits
         Customer c2 = new Customer("John Xina", 1828318, 0.30, 0.12);
-        c2.displayBalance(Customer.CHEQUING);
-        c2.displayBalance(Customer.SAVING);
+        c2.displayBalance(Customer.CHEQUING); //expected output: 0.30
+        c2.displayBalance(Customer.SAVING);   //expected output: 0.12
         c2.deposit(1231,date, Customer.SAVING );
         c2.deposit(1231,date, Customer.CHEQUING );
         c2.displayDeposits();
         c2.displayWithdraws();
-        c2.displayBalance(Customer.CHEQUING);
-        c2.displayBalance(Customer.SAVING);
+        c2.displayBalance(Customer.CHEQUING); //expected output: 1231.30
+        c2.displayBalance(Customer.SAVING);   //expected output: 1231.12
 
 
     }
